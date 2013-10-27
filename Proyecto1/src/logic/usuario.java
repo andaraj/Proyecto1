b5979723351b921ccf989b59879f44fb70060d00
andaraj <andaraj@live.com>
2013-10-26 23:33:39 -0600

    primeras clases de proyecto

---
+++ Proyecto1/src/logic/usuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+import java.util.ArrayList;
+/**
+ *
+ * @author Jaime Andara
+ */
+public class usuario {
+    protected ArrayList <String> nombre;
+    protected ArrayList <String> Apellido;
+
+    public usuario() {
+        //nombre <> = new ArrayList();
+        
+    }
+    
+}
